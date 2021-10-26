862ed9d1d145f363d7857fb4e8deab05f221268d
Сафронов Дмитрий Алексеевич <da_safronov@itlabs.univer.local>
2021-10-26 10:29:11 +0300

    Initial commit

---
+++ Нармодник/PageHelper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Controls;
+
+namespace Нармодник
+{
+    class PageHelper
+    {
+        public static Frame MainFrame;
+        public static deuser0Entities dbConnect = new deuser0Entities();
+    }
+}
---
+++ Нармодник/RegisterPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Нармодник
+{
+    /// <summary>
+    /// Логика взаимодействия для RegisterPage.xaml
+    /// </summary>
+    public partial class RegisterPage : Page
+    {
+        public RegisterPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Регистрация прошла успешно!");
+            PageHelper.MainFrame.Navigate(new SignInPage());
+        }
+    }
+}
